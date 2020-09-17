5285a23ff7d146b0a84fef008daa13eb31c96e3b
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-09-17 19:59:43 +0700

    learn variable in golang

---
+++ Variable/MultiVariable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		name    = "safrudin"
+		country = "indonesia"
+	)
+
+	fmt.Println(name)
+	fmt.Println(country)
+}
---
+++ Variable/Variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "safrudin"
+	fmt.Println(name)
+
+	name = "safrudin dan udin"
+	fmt.Println(name)
+
+	var friend = "none"
+	fmt.Println(friend)
+
+	enemy := "none"
+	fmt.Println(enemy)
+
+	enemy = "i don't know"
+	fmt.Println(enemy)
+}
