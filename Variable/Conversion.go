e64e2b620e5af1d97456c455e79e0172842d79b2
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-09-19 16:09:56 +0700

    learn data type conversion in go

---
+++ Variable/Conversion.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai32 int32 = 23434
+	var nilai64 int64 = int64(nilai32)
+
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+
+	var nama string = "azis"
+	var huruf = nama[0]
+	var Shuruf = string(huruf)
+
+	fmt.Println(nama)
+	fmt.Println(huruf)
+	fmt.Println(Shuruf)
+}
